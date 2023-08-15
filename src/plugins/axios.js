7be2415ca297dc9e27ef19acf366cfeb16720e19
detextre4 <detextre4@gmail.com>
2023-08-15 19:13:28 -0400

    updates

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,18 +1,16 @@
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { useStorage } from 'vue3-storage-secure'
 
 export default (app) => {
-  axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
+  // set default baseURL
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
+
+  // set default header
+  const tokenAuth = useStorage().getStorageSync("tokenAuth")
+  if (tokenAuth) axios.defaults.headers.common['Authorization'] = `Token ${tokenAuth}`
 
   app
   .use(VueAxios, axios)
   .provide('axios', app.config.globalProperties.axios)
 }
-
-// export default function ({ $axios, redirect }) {
-//   $axios.onError(err => {
-//     if (err.response.status === 500) {
-//       redirect('/sorry')
-//     }
-//   })
-// }
