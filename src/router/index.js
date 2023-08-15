193aa435e291c7d2dbdcd8a7bc7a501d78a877e0
detextre4 <detextre4@gmail.com>
2023-08-15 15:10:41 -0400

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,24 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import { useStorage } from "vue3-storage-secure";
+import { nextTick } from 'vue'
+
+const DEFAULT_TITLE = 'Vue3 detextre4';
 
 const routes = [
   // ? Default routes
   {
     path: '/',
     component: () => import('@/layouts/DefaultLayout.vue'),
+    meta: {
+      requiresAuth: true
+    },
     children: [
       {
         path: '',
         name: 'Home',
         component: () => import('@/views/Home.vue'),
+        meta: { title: `Home - ${DEFAULT_TITLE}` }
       },
     ],
   },
@@ -24,6 +32,7 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: () => import('@/views/login.vue'),
+        meta: { title: `Login - ${DEFAULT_TITLE}` }
       },
     ],
   },
@@ -33,5 +42,29 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 })
+
+
+router.beforeEach((to, from, next) => {
+  // does not require auth, make sure to always call next()!
+  if (!to.matched.some(record => record.meta.requiresAuth)) return next()
+
+  const tokenAuth = useStorage().getStorageSync("tokenAuth")
+
+  // go to wherever I'm going
+  if (tokenAuth) return next()
+
+  // this route requires auth, check if logged in
+  // if not, redirect to login page.
+  next({ name: 'Login' })
+})
+
+
+router.afterEach((to, from) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  nextTick(() => {
+      document.title = to.meta.title || DEFAULT_TITLE;
+  });
+});
 
 export default router
