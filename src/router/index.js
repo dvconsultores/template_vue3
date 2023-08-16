fb39ccfdb09dfcbbf24dd4400c1f23ce253bfe8b
detextre4 <detextre4@gmail.com>
2023-08-16 15:35:58 -0400

    updates public path

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,9 @@ const routes = [
 ]
 
 const router = createRouter({
+  mode: 'history',
   history: createWebHistory(process.env.BASE_URL),
+  base: process.env.BASE_URL,
   routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -3,41 +3,49 @@ import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // Utilities
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({ 
-      template: { transformAssetUrls }
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: {
-    'process.env': {},
-    __VUE_I18N_FULL_INSTALL__: true,
-    __VUE_I18N_LEGACY_API__: false,
-    __INTLIFY_PROD_DEVTOOLS__: false,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
+export default defineConfig((command, mode, ssrBuild) => {
+  const isProduction = process.env.NODE_ENV === 'production'
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const baseUrl = isProduction ? env.BASE_URL : env.VITE_BASE_URL
+
+  return {
+    base: baseUrl,
+    plugins: [
+      vue({ 
+        template: { transformAssetUrls }
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+      }),
     ],
-  },
-  server: {
-    port: 3000,
-  },
+    define: {
+      'process.env': {
+        BASE_URL: baseUrl,
+        API_URL: env.VITE_API_URL,
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+      ],
+    },
+    server: {
+      port: 3000,
+    },
+  }
 })
