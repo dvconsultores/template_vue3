8d93cb1c11ac864e5d39798dc4d376a7ca6ab425
detextre4 <detextre4@gmail.com>
2023-08-16 23:39:44 -0400

    toastification updates

--- src/plugins/v-otp-input.js
+++ src/plugins/v-otp-input.js
@@ -1,5 +1,5 @@
 import VOtpInput from 'vue3-otp-input';
-import '@/assets/styles/components/VOtpInput.scss'
+import '@/assets/styles/components/v-otp-input.scss'
 
 export default (app) => {
   app.component('VOtpInput', VOtpInput)
--- src/plugins/vue-toastification.js
+++ src/plugins/vue-toastification.js
@@ -1,6 +1,7 @@
 import Toast, { POSITION } from "vue-toastification";
 // Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
+// import "@/assets/styles/components/vue-toastification.scss";
 
 export default (app) => {
   // documentation: https://github.com/Maronato/vue-toastification/tree/next
