1897745f930495a3c8180f314dab1d32f359a80b
detextre4 <detextre4@gmail.com>
2023-08-17 13:48:55 -0400

    update functions

--- src/plugins/functions.js
+++ src/plugins/functions.js
@@ -1,3 +1,46 @@
-export function customFunction() {
-  console.log("customFunction");
+export function isArray(val) {
+  return val.constructor.name == 'Array'
+}
+
+export function isNumber(val) {
+  return typeof val == 'number'
+}
+
+export function isString(val) {
+  return typeof val == 'string'
+}
+
+export function toCssVal(value, unit = 'px') {
+  if (isString(value)) {
+    return value
+  } else if (isNumber(value)) {
+    return `${value}${unit}`
+  } else if (isArray(value)) {
+    const length = value.length
+
+    // helper
+    function setValue(val, i) {
+      if (isString(val)) return val
+
+      const def = `${val}${unit}`
+      switch (length) {
+        case 2: {
+          if (i === 1) return `${val}em`
+        }
+        case 3: {
+          if (i === 1) return `${val}vw`
+        } break;
+      }
+
+      return def
+    }
+
+    const formatValue = value.map((e, i) => setValue(e, i)).join(',')
+
+    switch (length) {
+      case 1: return setValue(value.at(0))
+      case 2: return `max(${formatValue})`
+      default: return `clamp(${formatValue})`
+    }
+  }
 }
